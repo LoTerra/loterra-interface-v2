0784f63a74cf882a5afcf0c72c1d2ebf18065393
Dams2 <damien-rojo@hotmail.fr>
2022-02-15 14:55:07 +0100

    add percentage

--- src/components/Spacewager/SpaceWagerCard.jsx
+++ src/components/Spacewager/SpaceWagerCard.jsx
@@ -1,16 +1,63 @@
-import React, { useState } from 'react'
+import React, { useEffect,useState } from 'react'
 import numeral from 'numeral';
 
+function enterUP() {
+
+}
+
+function enterDown() {
+
+}
 
 export default function SpaceWagerCard(props) {
 
-    const {price, click} = props;
+    const [percentageStatus, setPercentageStatus] = useState('')
+    const {price, percentage, click} = props;
+    const [formattedPercentage, setFormattedPercentage] = useState(0)
+
+    const getPercentage = async (price) => {
+        console.log(formattedPercentage)
+        console.log(percentage)
+        try {
+            if (percentage != 0) {
+                let status = ''
+                if (percentage > 0) {
+                    status = 'up'
+                    setFormattedPercentage('+' + numeral(percentage).format('0,0.00') + '%')
+                    setPercentageStatus(status)
+                } else if (percentage < 0) {
+                    status = 'down'
+                    setFormattedPercentage('-' + numeral(percentage).format('0,0.00') + '%')
+                    setPercentageStatus(status)
+                } else {
+                    status = ''
+                    setFormattedPercentage(numeral(percentage).format('0,0.00') + '%')
+                    setPercentageStatus('')
+                }
+            }
+            
+            return 
+        } catch(e){
+            console.log(e)
+        }
+    }
+
+    //Load on mount
+    useEffect(() =>  {       
+        const interval = setInterval(() => {
+            getPercentage(formattedPercentage)    
+          }, 1000);
+          return () => clearInterval(interval); 
+               
+    },[formattedPercentage])
 
     return (
        <div className="col-md-4">
            <div className="card spacewager-card">
                 <button className="btn btn-primary" onClick={() => click('up')}>Up</button>
+                <p className="my-2 fw-regular fs-2">Last price</p>
                 <p className="my-2 fw-bold fs-2">{numeral(price).format('0,0.000')}</p>
+                <p className={percentageStatus}>{formattedPercentage}</p>
                 <button className="btn btn-secondary" onClick={() => click('down')}>Down</button>
            </div>
        </div>
--- src/pages/SpaceWager.js
+++ src/pages/SpaceWager.js
@@ -10,45 +10,45 @@ export default () => {
     const terra = state.lcd_client
 
     const [lunaPrice, setLunaPrice] = useState(0)
+    const [lunaLockedPrice, setLunaLockedPrice] = useState(0)
+    const [lunaPricePercentage, setLunaPricePercentage] = useState(0)
     const [lunaStatus, setLunaStatus] = useState('')
     const [lunaPriceCounter, setLunaPriceCountdown] = useState(0)
 
     const api = new WasmAPI(terra.apiRequester)
 
+    function getPercentage(currentPrice) {
+        var percentageVariation = (numeral(currentPrice).format('0,0.000') - 55.910) / 55.910 * 100
+
+        return percentageVariation
+    }
 
     const getLunaPrice = async (price) => {
-
+        console.log('price')
         try {
             const contractConfigInfo = await api.contractQuery(state.lunaPoolAddress, {
-            pool: {},
+                pool: {},
             }) 
-            console.log('contractConfigInfo',contractConfigInfo)
             let ust = parseInt(contractConfigInfo.assets[1].amount)
             let luna = parseInt(contractConfigInfo.assets[0].amount)
             
             let luna_base_price = luna / ust
             if(parseFloat(luna_base_price) != parseFloat(price)){
-                console.log('check data', luna_base_price, price)
                 let status = ''
                 if(numeral(luna_base_price).format('0,0.000') > numeral(price).format('0,0.000')){
                     status = 'up'
-                    console.log('uppppp')
                 } else if(numeral(luna_base_price).format('0,0.000') < numeral(price).format('0,0.000')){
                     status = 'down'
-                    console.log('downnnnn')
                 }
                 setLunaPrice(luna_base_price)
                 setLunaStatus(status)
                 setLunaPriceCountdown(lunaPriceCounter + 1)
             } else {
-                let status = ''
-                console.log('equalll')
-                setLunaPrice(luna_base_price) 
+                setLunaPrice(luna_base_price)
                 setLunaStatus('')
             }
-            
-            console.log('lunaPrice',numeral(luna_base_price).format('0,0.000'))
-
+            console.log(luna_base_price)
+            setLunaPricePercentage(getPercentage(luna_base_price))
         } catch(e){
             console.log(e)
         }
@@ -76,7 +76,7 @@ export default () => {
                 <div className="row">
                 {[1,2,2].map((obj,k) => {
                     return (
-                        <SpaceWagerCard key={k} price={lunaPrice} click={(a) => triggerClick(a)}/>
+                        <SpaceWagerCard key={k} price={lunaPrice} percentage={lunaPricePercentage} click={(a) => triggerClick(a)}/>
                     )
                 })}
                 </div>
