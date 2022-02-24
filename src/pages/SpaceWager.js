3845e6263d60061de18bb93babf93a6b306090e4
0xantman <antho74830@gmail.com>
2022-02-24 21:37:45 +0100

    Remove short swipe preventing undesired move of the carousell

--- src/pages/SpaceWager.js
+++ src/pages/SpaceWager.js
@@ -369,6 +369,7 @@ export default () => {
             <div className="row mb-2 position-relative">
                     {spacewagerState.round > 0 &&
                     <Swiper
+                        shortSwipes= {false}
                     spaceBetween={-50}
                     navigation={{
                         nextEl: '.swiper-next',
