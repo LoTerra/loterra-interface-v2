cb5e8dcab3b3e2251247635690c22984b54f89b5
Dams2 <damien-rojo@hotmail.fr>
2022-03-02 22:21:41 +0100

    Polish

--- src/pages/Tokenomics.js
+++ src/pages/Tokenomics.js
@@ -2,47 +2,86 @@ import { LCDClient, WasmAPI } from '@terra-money/terra.js';
 import numeral from 'numeral';
 import { useStore } from '../store'
 import { Head } from 'react-static'
-import React, { useState, useEffect, useMemo, useRef } from 'react'
+import React, { useState, useEffect, useCallback, useRef } from 'react'
+import {ChartLine} from 'phosphor-react'
 
-
-function tokenInfo(){
-
-    let render = (
-        <tr>
-            <td className="text-center">LoTerra</td>
-            <td className="text-center">LOTA</td>
-            <td className="text-center">cw20</td>
-            <td className="text-center">6</td>
-            <td className="text-center">Utility</td>
-            <td className="text-center">Terra</td>
-        </tr>
-    )
-
-    return (
-        <>
-            { render }
-        </>
-    )
-}
-
-function tokenSupply(){
-
-    let render = (
-        <tr>
-            <td className="text-start">1,939,548.765705 LOTA</td>
-            <td className="text-center">0.0</td>
-            <td className="text-end">0.0</td>
-        </tr>
-    )
-
-    return (
-        <>
-            { render }
-        </>
-    )
-}
+const BURNED_LOTA = 4301383550000
 
 export default () => {
+
+    const { state, dispatch } = useStore()
+    const [lotaPrice, setLotaPrice] = useState(0)
+    const terra = state.lcd_client
+    const api = new WasmAPI(terra.apiRequester)
+    const loterra_contract_address = 'terra1q2k29wwcz055q4ftx4eucsq6tg9wtulprjg75w'
+    const loterra_pool_address = 'terra1pn20mcwnmeyxf68vpt3cyel3n57qm9mp289jta'
+
+    function tokenInfo(){
+        let render = (
+            <tr>
+                <td className="text-center">LoTerra</td>
+                <td className="text-center">LOTA</td>
+                <td className="text-center">cw20</td>
+                <td className="text-center">6</td>
+                <td className="text-center">Utility</td>
+                <td className="text-center">Terra</td>
+            </tr>
+        )
+
+        return (
+            <>
+                { render }
+            </>
+        )
+    }
+
+    const fetchContractQuery = useCallback(async () => {
+        try {
+            //Get current lota price
+            const currentLotaPrice = await api.contractQuery(
+                loterra_pool_address,
+                {
+                    pool: {},
+                },
+            )
+            setLotaPrice(currentLotaPrice)
+        } catch(e){
+            console.log(e)
+        }
+    }, [])
+
+    function circulatingSupply() {
+        let total =
+            (parseInt(state.tokenInfo.total_supply) - BURNED_LOTA) / 1000000
+        let daoFunds = parseInt(state.daoFunds / 1000000)
+        let sum = total - daoFunds
+        return sum
+    }
+
+    function daoFunds() {
+        let daoFunds = parseInt(state.daoFunds / 1000000)
+        return daoFunds
+    }
+
+    function totalSupply() {
+        let total =
+            (parseInt(state.tokenInfo.total_supply) - BURNED_LOTA) / 1000000
+        return total
+    }
+
+    function marketCap() {
+        if (lotaPrice.assets) {
+            let sum =
+                (lotaPrice.assets[1].amount / lotaPrice.assets[0].amount) *
+                circulatingSupply()
+            return sum
+        }
+    }
+
+    useEffect(() => {
+        fetchContractQuery()   
+    }, [fetchContractQuery])
+
     return (
         <>
             <div className="w-100 py-3 py-md-5 text-center">
@@ -50,47 +89,136 @@ export default () => {
                 <h2 className="my-2 fw-regular fs-6 mb-0">LOTA is the governance token of the LoTerra ecosystem gaming lottery, token give the power to vote proposals. LOTA holders are owners of the LoTerra ecosystem casino, their job is to ensure the ecosystem is working well, they can control lotteries configurations and get staking rewards for responsibilities and management.</h2>
             </div>
             <div className="container-fluid">
-                <div className="w-100 py-4">
+                <div className="card-body">
+                    <div className="col-12 text-center">
+                        <h3 className="fw-bold">Information</h3>
+                    </div>
                     <div className="row">
-                            <div className="col-12 text-start">
-                                <h3 className="fw-bold">Token info</h3>
+                        <div className="col-md-2 mb-3">
+                            <div className="lota-stats">
+                                <p>Name</p>
+                                <h5>
+                                    LoTerra
+                                </h5>
                             </div>
-                    </div>
-                    <div className="table-responsive">
-                        <table className="table text-white">
-                            <thead>
-                                <tr>
-                                    <th style={{minWidth:100}} className="text-center">Name</th>
-                                    <th style={{minWidth:100}} className="text-center">Symbol</th>
-                                    <th style={{minWidth:100}} className="text-center">Token type</th>
-                                    <th style={{minWidth:100}} className="text-center">Decimals</th>
-                                    <th style={{minWidth:100}} className="text-center">Role</th>
-                                    <th style={{minWidth:100}} className="text-center">Blockchain</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {tokenInfo()}
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-                <div className="w-100 py-4">
-                    <div className="col-12 text-start">
-                        <h3 className="fw-bold">Token supply</h3>
-                    </div>
-                    <div className="table-responsive">
-                        <table className="table text-white">
-                            <thead>
-                                <tr>
-                                    <th style={{minWidth:80}} className="text-star">Total supply</th>
-                                    <th style={{minWidth:80}} className="text-center">Circulating Supply</th>
-                                    <th style={{minWidth:80}} className="text-end">DAO funds</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {tokenSupply()}
-                            </tbody>
-                        </table>
+                        </div>
+                        <div className="col-md-2 mb-3">
+                            <div className="lota-stats">
+                                <p>Symbol</p>
+                                <h5>
+                                    LOTA
+                                </h5>
+                            </div>
+                        </div>
+                        <div className="col-md-2 mb-3">
+                            <div className="lota-stats">
+                                <p>Blockchain</p>
+                                <h5>
+                                    Terra
+                                </h5>
+                            </div>
+                        </div>
+                        <div className="col-md-2 mb-3">
+                            <div className="lota-stats">
+                                <p>Token Type</p>
+                                <h5>
+                                    cw20
+                                </h5>
+                            </div>
+                        </div>
+                        <div className="col-md-2 mb-3">
+                            <div className="lota-stats">
+                                <p>decimals</p>
+                                <h5>
+                                    6
+                                </h5>
+                            </div>
+                        </div>
+                        <div className="col-md-2 mb-3">
+                            <div className="lota-stats">
+                                <p>Role</p>
+                                <h5>
+                                    Utility
+                                </h5>
+                            </div>
+                        </div>
+                        <div className="col-12 text-center">
+                            <h3 className="fw-bold">Token Supply</h3>
+                        </div>
+                        <div className="col-md-6 mb-3">
+                            <div className="lota-stats mb-4 mb-md-0">
+                                {lotaPrice.assets && (
+                                    <>
+                                        <p>Total supply</p>
+                                        <h5>
+                                            {numeral(totalSupply()).format('0,0.00',)}
+                                            <span>LOTA</span>
+                                        </h5>
+                                    </>
+                                )}
+                            </div>
+                        </div>
+                        <div className="col-md-6 mb-3">
+                            <div className="lota-stats">
+                                <p>DAO funds</p>
+                                <h5>
+                                    {numeral(daoFunds()).format('0,0.00',)}
+                                    <span>LOTA</span>
+                                </h5>
+                            </div>
+                        </div>
+                        <div className="col-md-6 mb-3">
+                            <div className="lota-stats">
+                                <p>Circulating SUPPLY</p>
+                                <h5>
+                                    {numeral(circulatingSupply()).format('0,0.00',)}
+                                    <span>LOTA</span>
+                                </h5>
+                            </div>
+                        </div>
+                        <div className="col-md-6 mb-3">
+                            <div className="lota-stats">
+                                <p>Market Cap</p>
+                                <h5>
+                                    {numeral(marketCap()).format('0,0.00',)}
+                                    <span>UST</span>
+                                </h5>
+                            </div>
+                        </div>
+                        <div className="col-md-12 mb-3">
+                            <div className="lota-stats">
+                                {lotaPrice.assets && (
+                                    <>
+                                        <p>Price</p>
+                                        <h5>
+                                            {numeral(lotaPrice.assets[1].amount / lotaPrice.assets[0].amount,).format('0.000')}
+                                            <span>UST</span>
+                                        </h5>
+                                    </>
+                                )}
+                            </div>
+                        </div>
+                        <div className="col-md-12 text-center">
+                            <a
+                                href="https://coinhall.org/charts/terra/terra1pn20mcwnmeyxf68vpt3cyel3n57qm9mp289jta"
+                                target="_blank"
+                                className="btn btn-plain"
+                                style={{
+                                    color: 'rgb(166, 159, 187)',
+                                    fontSize: '16px',
+                                }}
+                            >
+                                <ChartLine
+                                    size={21}
+                                    style={{
+                                        position: 'relative',
+                                        top: '-2px',
+                                        marginRight: '3px',
+                                    }}
+                                />
+                                View chart
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
