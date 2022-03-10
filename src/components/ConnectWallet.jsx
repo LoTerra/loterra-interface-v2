ff6c3a28596812ee682fe38d72b150d27cc508db
0xantman <antho74830@gmail.com>
2022-03-10 17:54:47 +0100

    Fix

--- src/components/ConnectWallet.jsx
+++ src/components/ConnectWallet.jsx
@@ -41,6 +41,7 @@ const DialogButton = {
     margin: '10px 20px 10px 20px',
 }
 export default function ConnectWallet() {
+
     let connectedWallet = ''
     const [isDisplayDialog, setIsDisplayDialog] = useState(false)
     const [isModal, setIsModal] = useState(false)
--- src/pages/Rapido.js
+++ src/pages/Rapido.js
@@ -179,6 +179,7 @@ export default () => {
         setInterval(() => {
             formatTime()
         }, 1000)
+
     },[])
 
     useEffect(() =>{
